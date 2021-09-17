7c36bbecc6246a001bcb726ff6c81b0e212e1ac9
manuel iglesias <m.iglesias91@gmail.com>
2021-09-17 17:52:52 -0300

    probando ejercicios

--- ejercicios/01_tipos_de_datos_basicos/02_numeros_test.py
+++ ejercicios/01_tipos_de_datos_basicos/02_numeros_test.py
@@ -1,13 +1,63 @@
 import pytest
+import inspect
 
-def test_suma_igual_a_10():
-    un_numero = 6
-    otro_numero = 4
+respuesta = None
+resultado = None
+solucion = None
 
-    assert un_numero + otro_numero == 10
+def test_sumar_numeros():
+    # - sumar dos numeros, y
+    # - que la suma sea igual a 10
 
-def test_resta_igual_a_20():
-    un_numero = 0
-    otro_numero = 0
+    respuesta = '''
+    5 + 5
+    '''
+    
+    assert '+' in respuesta
+    assert eval(respuesta.strip()) == 10
 
-    assert un_numero - otro_numero == 20
+def test_sumar_variables():
+    # - crear las variables 'una_variable' y 'otra_variable'
+    # - sumarlas, y
+    # - que la suma sea igual a 100
+
+    respuesta = '''
+una_variable = 5
+otra_variable = 95
+resultado = una_variable + otra_variable
+'''
+
+    exec(respuesta)
+    assert '+' in respuesta
+    assert 'una_variable' in respuesta
+    assert 'otra_variable' in respuesta
+    assert resultado == 100
+
+def test_restar_numeros():
+    # - restar dos numeros,
+    # - que la resta sea igual a 10, y
+    # - guardar la resta en la variable 'resultado'
+    
+    respuesta = '''
+    20 - 10
+    '''
+
+    # eval(respuesta)
+    assert '-' in respuesta
+    assert eval(respuesta.strip()) == 10
+
+def test_restar_variables():
+    # - crear las variables 'una_variable' y 'otra_variables'
+    # - restarlas, 
+    # - que la resta sea igual a 100, y 
+    # - guardar la resta en la variable 'resultado'
+
+    respuesta = '''
+    (acá escribir código)
+    '''
+
+    exec(respuesta)
+    assert '-' in respuesta
+    assert 'una_variable' in respuesta
+    assert 'otra_variable' in respuesta
+    assert resultado == 100
